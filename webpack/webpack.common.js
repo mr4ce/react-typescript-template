818cb81a4b6ffca1e267ade45d4631a6a7377d06
Vitaly Diachenko <v.di4chenko@gmail.com>
2021-03-28 16:49:05 +0300

    [webpack] Add required loaders

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,6 +8,13 @@ module.exports = {
       {
         oneOf: [
           {
+            test: /\.css$/i,
+            use: [
+              { loader: 'css-loader' },
+              { loader: 'postcss-loader' },
+            ],
+          },
+          {
             test: /\.less$/i,
             use: [
               {
@@ -30,7 +37,33 @@ module.exports = {
             ],
           },
           {
-            test: /\.tsx?$/,
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 1000,
+                  name: 'static/images/[name].[hash:8].[ext]',
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(eot|ttf|woff)/i,
+            loader: 'file-loader',
+            options: {
+              name: 'static/fonts/[name].[hash:8].[ext]',
+            },
+          },
+          {
+            loader: 'file-loader',
+            exclude: /\.(js|json|html|mjs|jsx|ts|tsx)$/i,
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+          {
+            test: /\.tsx?$/i,
             use: 'ts-loader',
             exclude: /node_modules/,
           },
@@ -38,6 +71,14 @@ module.exports = {
       }
     ],
   },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(process.cwd(), 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'public/index.html' })
+  ],
   resolve: {
     alias: {
       '~': path.resolve(process.cwd(), './src'),
@@ -48,12 +89,4 @@ module.exports = {
       path.resolve('./src'),
     ]
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(process.cwd(), 'dist'),
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'public/index.html' })
-  ]
 };
